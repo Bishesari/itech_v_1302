d9f0d21ccec83049182badbe4feeab7b1a60246d
SiteD-Server <Yasser.Bishesari@gmail.com>
2026-06-05 14:14:06 +0330

    Seeding

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\City;
+use App\Models\Province;
 use Illuminate\Database\Seeder;
 
 class CitySeeder extends Seeder
@@ -12,6 +13,35 @@ class CitySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $cities = [
+            'bushehr' => [
+                ['name' => 'بوشهر', 'slug' => 'bushehr'],
+                ['name' => 'گناوه', 'slug' => 'genaveh'],
+                ['name' => 'برازجان', 'slug' => 'borazjan'],
+            ],
+
+        ];
+
+        foreach ($cities as $provinceSlug => $cityList) {
+
+            $province = Province::where('slug', $provinceSlug)->first();
+
+            if (! $province) {
+                continue;
+            }
+
+            foreach ($cityList as $city) {
+
+                City::updateOrCreate(
+                    [
+                        'province_id' => $province->id,
+                        'name' => $city['name'],
+                    ],
+                    [
+                        'slug' => $city['slug'],
+                    ]
+                );
+            }
+        }
     }
 }
--- database/seeders/InstituteSeeder.php
+++ database/seeders/InstituteSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Institute;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,34 @@ class InstituteSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $institutes = [
+
+            [
+                'short_name' => 'آی تک',
+                'full_name' => 'آموزشگاه فنی و حرفه‌ای آزاد آی تک',
+                'slug' => 'I-Tech Institute',
+                'abbr' => 'ITCBU',
+                'is_active' => true,
+                'logo_url' => null,
+            ],
+
+            [
+                'short_name' => 'البرز',
+                'full_name' => 'آموزشگاه فنی البرز',
+                'slug' => 'alborz-tech',
+                'abbr' => 'ALB',
+                'is_active' => true,
+                'logo_url' => null,
+            ],
+
+        ];
+
+        foreach ($institutes as $institute) {
+
+            Institute::updateOrCreate(
+                ['slug' => $institute['slug']],
+                $institute
+            );
+        }
     }
 }
