328f38871f04d1a8ab1637715f68d9fa0e99793d
C-Server <Yasser.Bishesari@gmail.com>
2026-06-06 15:26:07 +0330

    user_name instead email and RTL Layout

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -10,16 +10,16 @@
         <form method="POST" action="{{ route('login.store') }}" class="flex flex-col gap-6">
             @csrf
 
-            <!-- Email Address -->
+            <!-- User Name -->
             <flux:input
-                name="email"
-                :label="__('Email address')"
-                :value="old('email')"
-                type="email"
+                name="user_name"
+                :label="__('User Name')"
+                :value="old('user_name')"
+                type="text"
                 required
                 autofocus
-                autocomplete="email"
-                placeholder="email@example.com"
+                autocomplete="user_name"
+                placeholder="User Name"
             />
 
             <!-- Password -->
